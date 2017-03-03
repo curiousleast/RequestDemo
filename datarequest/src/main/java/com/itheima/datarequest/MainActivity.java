9a5d23c757281c91ab144416a7b634928fe2e189
Tracy_xq <530904047@qq.com>
2017-03-03 14:08:21 +0800

    third commit

--- datarequest/src/main/java/com/itheima/datarequest/MainActivity.java
+++ datarequest/src/main/java/com/itheima/datarequest/MainActivity.java
@@ -2,6 +2,7 @@ package com.itheima.datarequest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //内存缓存cookie,退出应用后cookie被清理
+    }
+
+    //双击退出
+
+    int clickCount = 0;
+
+    @Override
+    public void onBackPressed() {
+        if (clickCount < 1){
+            Toast.makeText(this,"再次点击退出",0).show();
+            clickCount++;
+        }
+        finish();
     }
 }
